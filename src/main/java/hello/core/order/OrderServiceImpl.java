bc2c464c51af95874b4e4e9737785c8a33ec34f5
Taewon Yoon <Tonyzorz@naver.com>
2022-01-07 17:09:35 +0900

    섹션 7. 의존관계 자동 주입 : 생성자 주입을 선택해라!

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -12,10 +12,13 @@ public class OrderServiceImpl implements OrderService{
 
     /* 필드 의존성 주입은 외부에서 변경이 불가능
     @Autowired */
-    private MemberRepository memberRepository;
-    private DiscountPolicy discountPolicy;
+    /*
+    final 키워드 사용시 생성자에게 혹시라도 값이 설정이 안되었을시 오류를 컴파일 시점에 막아준다
+     */
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
 
-    @Autowired
+/*    @Autowired
     public void setMemberRepository(MemberRepository memberRepository) {
         System.out.println("memberRepository = " + memberRepository);
         this.memberRepository = memberRepository;
@@ -25,9 +28,9 @@ public class OrderServiceImpl implements OrderService{
     public void setDiscountPolicy(DiscountPolicy discountPolicy) {
         System.out.println("discountPolicy = " + discountPolicy);
         this.discountPolicy = discountPolicy;
-    }
+    }*/
 
-    @Autowired //필수, 불변
+    //@Autowired //필수, 불변
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         System.out.println("1. OrderServiceImpl.OrderServiceImpl");
         this.memberRepository = memberRepository;
